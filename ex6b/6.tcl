fe2919afda3533fa8bc0e309524665885689a753
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-12-14 21:05:27 +0530

    Update 6.tcl

--- ex6b/6.tcl
+++ ex6b/6.tcl
@@ -1,1 +1,39 @@
-blank
+set ns [new Simulator]
+set nf [open prog5.nam w]
+$ns namtrace-all $nf
+set nd [open prog5.tr w]
+$ns trace-all $nd
+
+proc finish {} {
+global ns nf nd
+$ns flush-trace
+close $nf
+close $nd
+exec nam prog5.nam &
+exit 0
+}
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+set n6 [$ns node]
+
+$ns make-lan "$n0 $n1 $n2 $n3 $n4 $n5 $n6" 0.2Mb 40ms LL Queue/DropTail
+Mac/802_3
+
+set tcp [new Agent/TCP]
+$ns attach-agent $n0 $tcp
+set sink [new Agent/TCPSink]
+$ns attach-agent $n5 $sink
+$ns connect $tcp $sink
+
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+
+$ns at 1.0 "$ftp start"
+$ns at 5.0 "$ftp stop"
+$ns at 5.5 "finish"
+$ns run
