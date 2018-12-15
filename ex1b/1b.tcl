7f847cfd209fc2fe4c13a38df8d44271f88b3f9b
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-12-15 19:35:17 +0530

    Update 1b.tcl

--- ex1b/1b.tcl
+++ ex1b/1b.tcl
@@ -13,6 +13,8 @@ exec nam prog1.nam &
 exit 0
 }
 
+#creating the nodes and setting the links
+
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
@@ -21,12 +23,15 @@ $ns duplex-link $n0 $n1 1Mb 20ms DropTail
 $ns duplex-link $n1 $n2 512Kb 20ms DropTail
 $ns queue-limit $n1 $n2 5
 
+# creating various agents and attaching them with nodes
 set udp0 [new Agent/UDP]
 $ns attach-agent $n0 $udp0
+
 set cbr0 [new Application/Traffic/CBR]
 $cbr0 set packetSize_ 500
 $cbr0 set interval_ 0.005
 $cbr0 attach-agent $udp0
+
 set sink [new Agent/Null]
 $ns attach-agent $n2 $sink
 $ns connect $udp0 $sink
