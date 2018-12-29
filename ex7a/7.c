1b4f09991d4f05c99a5bcc22e9b00e86be4b73d6
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-12-29 19:35:59 +0530

    Update 7.c

--- ex7a/7.c
+++ ex7a/7.c
@@ -1,145 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 5
-void insert_by_priority(int);
-void delete_by_priority(int);
-void create();
-void check(int);
-void display_pqueue();
-// Need to add HP and LP
- 
-int pri_que[MAX];
-int front, rear;
- 
-void main()
-{
-    int n, ch;
- 
-    printf("\n1 - Insert an element into queue");
-    printf("\n2 - Delete an element from queue");
-    printf("\n3 - Display queue elements");
-    printf("\n4 - Exit");
- 
-    create();
- 
-    while (1)
-    {
-        printf("\nEnter your choice : ");    
-        scanf("%d", &ch);
- 
-        switch (ch)
-        {
-        case 1: 
-            printf("\nEnter value to be inserted : ");
-            scanf("%d",&n);
-            insert_by_priority(n);
-            break;
-        case 2:
-            printf("\nEnter value to delete : ");
-            scanf("%d",&n);
-            delete_by_priority(n);
-            break;
-        case 3: 
-            display_pqueue();
-            break;
-        case 4: 
-            exit(0);
-        default: 
-            printf("\nChoice is incorrect, Enter a correct choice");
-        }
-    }
-}
- 
-/* Function to create an empty priority queue */
-void create()
-{
-    front = rear = -1;
-}
- 
-/* Function to insert value into priority queue */
-void insert_by_priority(int data)
-{
-    if (rear >= MAX - 1)
-    {
-        printf("\nQueue overflow no more elements can be inserted");
-        return;
-    }
-    if ((front == -1) && (rear == -1))
-    {
-        front++;
-        rear++;
-        pri_que[rear] = data;
-        return;
-    }    
+#include<stdio.h>
+int high[25]={0},low[25]={0};
+int hf=0,lf=0,hr=0,lr=0;
+
+void insert(int val,int pri){
+  if(pri==1){
+    if(hr<24){
+    printf("Packet inserted into high priority queue\n");
+    high[hr++]=val;
+  }
     else
-        check(data);
-    rear++;
+    printf("The queue is full\n");
+  }
+  else if(pri==2){
+   if(lr<24){
+    printf("Packet inserted into low priority queue\n");
+    low[lr++]=val;
+  }
+   else
+   printf("Queue is full\n");
+  }
+  else
+    printf("Invalid priority request\n");
 }
- 
-/* Function to check priority and place element */
-void check(int data)
-{
-    int i,j;
- 
-    for (i = 0; i <= rear; i++)
-    {
-        if (data >= pri_que[i])
-        {
-            for (j = rear + 1; j > i; j--)
-            {
-                pri_que[j] = pri_que[j - 1];
-            }
-            pri_que[i] = data;
-            return;
-        }
-    }
-    pri_que[i] = data;
+
+void process(){
+  if(hf!=hr){
+    printf("Processing high prio-rity queue\n");
+   
+      printf("%d\t",high[hf++]);
+    
+  }
+  else{
+    if(lf!=lr)
+     {printf("Processing low priority queue\n");   
+    
+      printf("%d\t",low[lf++]);
+   } 
+  }
+  printf("\nProcess complete\n"); 	
 }
- 
-/* Function to delete an element from queue */
-void delete_by_priority(int data)
-{
-    int i;
- 
-    if ((front==-1) && (rear==-1))
-    {
-        printf("\nQueue is empty no elements to delete");
-        return;
+
+void main(){
+  int val,pri,ch=1;
+  while(ch!=3){
+    printf("Enter 1 for insertion,2 to process, 3 to exit\n");
+    scanf("%d",&ch);
+    if(ch==1){
+      //printf("Enter the packet value and priority\n");
+      printf("add value");
+      scanf("%d",&val);
+      printf("priority");
+      scanf("%d",&pri);
+      insert(val,pri);
     }
- 
-    for (i = 0; i <= rear; i++)
-    {
-        if (data == pri_que[i])
-        {
-            for (; i < rear; i++)
-            {
-                pri_que[i] = pri_que[i + 1];
-            }
- 
-        pri_que[i] = -99;
-        rear--;
- 
-        if (rear == -1) 
-            front = -1;
-        return;
-        }
+    else if(ch==2){
+      process();
     }
-    printf("\n%d not found in queue to delete", data);
-}
- 
-/* Function to display queue elements */
-void display_pqueue()
-{
-    if ((front == -1) && (rear == -1))
-    {
-        printf("\nQueue is empty");
-        return;
-    }
- 
-    for (; front <= rear; front++)
-    {
-        printf(" %d ", pri_que[front]);
-    }
- 
-    front = 0;
+    else
+      break;
+  }
 }
