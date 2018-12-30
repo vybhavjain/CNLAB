a7f5796058115396a9653cb19f3cc45d2b60147b
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-12-30 14:57:55 +0530

    Update 5.c

--- ex5a/5.c
+++ ex5a/5.c
@@ -1,125 +1,67 @@
-// Q)
-
-
-
+// Q) CRC 16-bits technique
+ 
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
-#include<math.h>
-#include<time.h>
-
-void divide(char agdtw[],char divs[],char remd[])
-
-{
-    int i,r,l,a,t;
-    r=strlen(divs);
-    t=strlen(agdtw)-r+1;
-    char divd[18],rem[18];
-    strncpy(divd,agdtw,r);
-    divd[r]='\0';
-    l=0;
-    memset(rem, 0,18);
-    while(l<t)
-    {
-        a=0;
-        memset(rem, 0,18);
-        if(divd[0]==divs[0])
-        {
-            for(i=1;i<r;i++)
-            {
-                if(divd[i]==divs[i])
-                    rem[a++]='0';
-                else
-                    rem[a++]='1';
-            }
-            rem[a]='\0';
-            strcpy(divd,rem);
-        }
-        else
-        {
-            strncpy(divd,&divd[1],strlen(divd)-1);
-            divd[r-1]='\0';
-        }
-        int o=strlen(divd);
-        divd[o]=agdtw[l+r];
-        divd[r]='\0';
-        l++;
-    }
-
-    strncpy(remd,divd,r-1);
-    remd[r-1]='\0';
+#define N strlen(g)
+ 
+char t[28],cs[28],g[]="10001000000100001";
+int a,e,c;
+ 
+void xor(){
+    for(c = 1;c < N; c++)
+    cs[c] = (( cs[c] == g[c])?'0':'1');
 }
-
-void binary(char letter,char bin[])
-{
-    int t,c,i=7;
-    c=(int)letter;
-    while(i>=0)
-    {
-        t=c%2;
-        c=c/2;
-        bin[i--]=t+'0';
-    }
-    bin[8]='\0';
+ 
+void crc(){
+    for(e=0;e<N;e++)
+        cs[e]=t[e];
+    do{
+        if(cs[0]=='1')
+            xor();
+        for(c=0;c<N-1;c++)
+            cs[c]=cs[c+1];
+        cs[c]=t[e++];
+    }while(e<=a+N-1);
 }
-
-char ascii(char bin[])
+ 
+int main()
 {
-    int t=0,c,i=7;
-    while(i>=0)
-    {
-        t=t+pow(2,7-i)*(bin[i]-'0');
-        i--;
-    }
-    return t;
-}
-
-void main()
-{    
-char dw[126],augdw[1018],div[18],rem[18],cw[1018],rcw[1018],bin[9],rdw[1001],msg[126];
-    printf("Enter a Message to be sent (Max 125 Char)\n");
-    fgets(dw, sizeof(dw), stdin);
-    binary(dw[0],bin);
-    strcpy(augdw,bin);
-    int j,k,e;
-    for(j=1;j<strlen(dw);j++)
-    {
-        binary(dw[j],bin);
-        strcat(augdw,bin);
-    }
-    strcat(augdw,"0000000000000000");
-
-    printf("\nEnter Divisor (generator) of 17 bits\n");
-    scanf("%s",div);
-    divide(augdw,div,rem);
-    strcpy(cw,augdw);
-    strcpy(&cw[strlen(augdw)-16],rem);
-    strcpy(rcw,cw);
-    printf("\nEnter no. of errors to be introduced during transmission :");
+    printf("\nEnter data : ");
+    scanf("%s",t);
+    printf("\n----------------------------------------");
+    printf("\nGeneratng polynomial : %s",g);
+    a=strlen(t);
+    for(e=a;e<a+N-1;e++)
+        t[e]='0';
+    printf("\n----------------------------------------");
+    printf("\nModified data is : %s",t);
+    printf("\n----------------------------------------");
+    crc();
+    printf("\nChecksum is : %s",cs);
+    for(e=a;e<a+N-1;e++)
+        t[e]=cs[e-a];
+    printf("\n----------------------------------------");
+    printf("\nFinal codeword is : %s",t);
+    printf("\n----------------------------------------");
+    printf("\nTest error detection 0(yes) 1(no)? : ");
     scanf("%d",&e);
-    srand(time(0));
-    for(j=0;j<e;j++)
+    if(e==0)
     {
-        k=rand()%strlen(rcw)-1;
-        if(rcw[k]=='0')
-            rcw[k]='1';
+        do{
+            printf("\nEnter the position where error is to be inserted : ");
+            scanf("%d",&e);
+        }while(e==0 || e>a+N-1);
+        t[e-1]=(t[e-1]=='0')?'1':'0';
+        printf("\n----------------------------------------");
+        printf("\nErroneous data : %s\n",t);
+    }
+    crc();
+    for(e=0;(e<N-1) && (cs[e]!='1');e++);
+        if(e<N-1)
+            printf("\nError detected\n\n");
         else
-            rcw[k]='0';
-        printf("Error Generated at %d th bit %d thcharacter\n",k,(k/8)+1);
-    }
-    divide(rcw,div,rem);
-    if(strcmp(rem,"0000000000000000")!=0)
-        printf("\n\nErroneous Transmission detected!\n");
-    strncpy(rdw,rcw,strlen(rcw)-16);
-    rdw[strlen(rcw)-16]='\0';
-    for(j=0,k=0;j<strlen(rdw);j=j+8)
-    {
-        strncpy(bin,&rdw[j],8);
-        bin[8]='\0';
-        msg[k++]=ascii(bin);
-    }
-    msg[k]='\0';
-    printf("\nRecieved Message = %s\n\n",msg);
+            printf("\nNo error detected\n\n");
+            printf("\n----------------------------------------\n");
+        return 0;
 }
 
-gcc prog5.c -lm
